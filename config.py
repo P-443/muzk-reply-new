94004181a52eafc71a15344c77dc09f169c0e8e8
P-443 <noreply@users.noreply.github.com>
2026-08-29 09:50:31 +0300

    chore: replace bot image with the one from t.me/Depr_essi_on/63 (telesco.pe CDN)

--- config.py
+++ config.py
@@ -13,8 +13,9 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
 OWNER_ID = int(getenv("OWNER_ID"))
 
-PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/3e5039ee25df9814417c8.jpg")
-START_IMG = getenv("START_IMG", "https://telegra.ph/file/3e5039ee25df9814417c8.jpg")
+_DEFAULT_IMG = "https://cdn4.telesco.pe/file/JHOKRszNSJK6ucc7QS-8kpvfD1fl62_DmB9QyPjP4s5IWxgUI_rCk6C5XXjyQSiKlsMy94O89H0xNR9-wN0PgpMDWdKDEcK0wWSG3_lkh-6x73_yMSNucwBXU6rivUsNWyanXjz4kOzsc9nxXKzMbSJIxv4Rjwbq78l6gMQBZ9HEPkoDT48BmlKFqBYE1tVXST5IPc3CAMNxcB5e6F3ejwdQdK1vu8vH6_Y3waLCPrHKvitwXC0JRdA1LWbrd6irk1OKIsA4XqpFLtZraEdpa9ZKDhsNxWK9r9rCJuaXkZmSJmOR10sMNYetkv_mvi4WVCp8_tVk-A0pEhkGXN_g0w"
+PING_IMG = getenv("PING_IMG", _DEFAULT_IMG)
+START_IMG = getenv("START_IMG", _DEFAULT_IMG)
 
 SESSION = getenv("SESSION", None)
 
@@ -24,4 +25,4 @@ SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/b1o_d_a")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "6264668799").split()))
 
 
-FAILED ="https://telegra.ph/file/3e5039ee25df9814417c8.jpg"
+FAILED = _DEFAULT_IMG
